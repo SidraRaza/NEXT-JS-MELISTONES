5268ff9e6c9f06e6881a04c17707dae8b469143e
SidraRaza <160983959+SidraRaza@users.noreply.github.com>
2024-09-13 20:48:23 +0500

    first commit

---
+++ milestone 1/src/app/page.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+
+export default function Home() {
+  return (
+    <main>
+    
+      
+      <div>
+        <h1 className="text-3xl font-bold text-center text-red-400 py-20">Hello, World!</h1>
+      </div>
+
+    </main>
+  );
+}
