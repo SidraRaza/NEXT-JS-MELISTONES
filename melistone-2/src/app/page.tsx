78730182b9dd1abce668c2ac69e8507604d9803e
SidraRaza <160983959+SidraRaza@users.noreply.github.com>
2024-09-15 00:00:54 +0500

    first commit

--- melistone-2/src/app/page.tsx
+++ melistone-2/src/app/page.tsx
@@ -7,28 +7,22 @@ export default function Home() {
     <>
       <Header />
       <div className="bg-[url('/12.jpg')] bg-cover bg-center bg-no-repeat relative">
-<<<<<<< HEAD:melistone-2/src/app/page.tsx
-<<<<<<< HEAD:melistone-2/src/app/page.tsx
         <div className="relative min-h-[550px] w-full h-auto md:h-[750px] p-4 md:p-10 flex justify-center items-center text-center z-0">
-=======
-        <div className="relative min-h-[650px]  sm:h-[750px] w-full h-auto md:h-[550px] p-4 md:p-10 flex justify-center items-center text-center z-0">
->>>>>>> dabba2c032e8522fdc02b12ae5bca8143041e493:project-2/src/app/page.tsx
-=======
-        <div className="relative min-h-[650px]  sm:h-[750px] w-full h-auto md:h-[550px] p-4 md:p-10 flex justify-center items-center text-center z-0">
->>>>>>> dabba2c032e8522fdc02b12ae5bca8143041e493:project-2/src/app/page.tsx
-          <div className="relative z-10">
-            <h1 className="text-2xl sm:text-3xl md:text-4xl font-bold  md:pt-10 bg-gradient-to-r from-yellow-400 to-orange-700 bg-clip-text text-transparent">
-              Hello, I Am Sidra Raza
-            </h1>
-            <p className="sm:text-lg md:text-2xl text-white pt-4 md:pt-10 text-justify lg:px-20 md:px-10">
-            &quot;Hello! I&apos;m a web developer and future Generative Cloud AI Engineer with a passion for building cutting-edge solutions. 
-             With expertise in web development and a keen interest in AI and cloud technology, I&apos;m always looking to push the boundaries
-             of what&apos;s possible. Plus, I&apos;m also a graphic designer, bringing a creative touch to my work. Let&apos;s explore the intersection 
-              of technology and innovation together!&quot;</p>
+          <div className="relative min-h-[650px] sm:h-[750px] w-full h-auto md:h-[550px] p-4 md:p-10 flex justify-center items-center text-center z-0">
+            <div className="relative z-10">
+              <h1 className="text-2xl sm:text-3xl md:text-4xl font-bold md:pt-10 bg-gradient-to-r from-yellow-400 to-orange-700 bg-clip-text text-transparent">
+                Hello, I Am Sidra Raza
+              </h1>
+              <p className="sm:text-lg md:text-2xl text-white pt-4 md:pt-10 text-justify lg:px-20 md:px-10">
+                &quot;Hello! I&apos;m a web developer and future Generative Cloud AI Engineer with a passion for building cutting-edge solutions. 
+                With expertise in web development and a keen interest in AI and cloud technology, I&apos;m always looking to push the boundaries
+                of what&apos;s possible. Plus, I&apos;m also a graphic designer, bringing a creative touch to my work. Let&apos;s explore the intersection 
+                of technology and innovation together!&quot;
+              </p>
+            </div>
           </div>
         </div>
       </div>
-      
     </>
   );
 }
